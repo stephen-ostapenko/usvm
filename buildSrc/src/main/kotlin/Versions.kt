7cbe6532dfe8645d05a896a9e44069655eb13f8d
Alexey Menshutin <alex.menshutin99@gmail.com>
2024-04-25 11:17:03 +0300

    Samples and test resolver changes

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,5 +18,5 @@ object Versions {
     const val rd = "2023.2.0"
     const val ini4j = "0.5.4"
 
-    const val arktsJacoDBVersion = "a23878503d"
+    const val arktsJacoDBVersion = "b58907954b"
 }
